e916ab5120a5f763c412d270595f869a625e3b5e
Alissa <alissayarmantho1@gmail.com>
2021-10-19 15:30:36 +0800

    Add Find command for recipes based on the recipe name - Edit find command for ingredients to add the ingredient keyword differentiate between recipe and ingredient - Add plural customisation for find success message for both commands

---
+++ src/main/java/fridgy/logic/commands/recipe/FindRecipeCommand.java
@@ -0,0 +1,42 @@
+package fridgy.logic.commands.recipe;
+
+import static java.util.Objects.requireNonNull;
+
+import fridgy.commons.core.Messages;
+import fridgy.logic.commands.CommandResult;
+import fridgy.model.RecipeModel;
+import fridgy.model.recipe.NameContainsKeywordsPredicate;
+
+public class FindRecipeCommand extends RecipeCommand{
+    public static final String COMMAND_WORD = "find";
+    public static final String RECIPE_KEYWORD = "recipe";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " " + RECIPE_KEYWORD +  ": Finds all recipes whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " " + RECIPE_KEYWORD + " " + " burger salad tempura";
+
+    private final NameContainsKeywordsPredicate predicate;
+
+    public FindRecipeCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(RecipeModel model) {
+        requireNonNull(model);
+        model.updateFilteredRecipeList(predicate);
+
+        int size = model.getFilteredRecipeList().size();
+        String plural = size == 0 || size == 1 ? "" : "s";
+        return new CommandResult(
+                String.format(Messages.MESSAGE_RECIPES_LISTED_OVERVIEW, size, plural));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindRecipeCommand // instanceof handles nulls
+                && predicate.equals(((FindRecipeCommand) other).predicate)); // state check
+    }
+}
---
+++ src/main/java/fridgy/model/recipe/NameContainsKeywordsPredicate.java
@@ -0,0 +1,31 @@
+package fridgy.model.recipe;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import fridgy.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Ingredient}'s {@code Name} matches any of the keywords given.
+ */
+public class NameContainsKeywordsPredicate implements Predicate<Recipe> {
+    private final List<String> keywords;
+
+    public NameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Recipe recipe) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(recipe.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
