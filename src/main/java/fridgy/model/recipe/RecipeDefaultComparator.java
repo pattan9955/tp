2ec42228b07231e76795632a739d8f6a71872222
Alissa <alissayarmantho1@gmail.com>
2021-10-20 02:51:26 +0800

    Filter away expired ingredients in recipe comparator

--- src/main/java/fridgy/model/recipe/RecipeDefaultComparator.java
+++ src/main/java/fridgy/model/recipe/RecipeDefaultComparator.java
@@ -1,5 +1,7 @@
 package fridgy.model.recipe;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -28,10 +30,16 @@ public class RecipeDefaultComparator implements Comparator<Recipe> {
      */
     @Override
     public int compare(Recipe i1, Recipe i2) {
+        LocalDate currentDate = LocalDate.now();
+
         Set<RecipeIngredient> ingredientsList1 = i1.getIngredients();
         Set<RecipeIngredient> ingredientsList2 = i2.getIngredients();
 
         List<String> ingredientNames = model.getFilteredIngredientList().stream()
+                .filter((ingredient) -> {
+                    Period periodToExpiry = Period.between(currentDate, ingredient.getExpiryDate().expiryDate);
+                    return !periodToExpiry.isNegative();
+                })
                 .map((ingredient)-> ingredient.getName().fullName).collect(Collectors.toList());
 
         Long noOfMissingIngredients1 = ingredientsList1.stream().map((recipeIngredient) -> recipeIngredient.getName())
