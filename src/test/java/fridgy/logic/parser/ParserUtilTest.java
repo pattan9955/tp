af063365407da65a515b386148fece24fddd5832
bailin <rkxuanfeng@gmail.com>
2021-10-25 13:08:27 +0800

    Remove unused imports

--- src/test/java/fridgy/logic/parser/ParserUtilTest.java
+++ src/test/java/fridgy/logic/parser/ParserUtilTest.java
@@ -1,7 +1,6 @@
 package fridgy.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
