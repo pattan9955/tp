0b5788b587203a961424c7f649e0f2723ad42c91
Alissa <alissayarmantho1@gmail.com>
2021-10-27 05:44:51 +0800

    Add tag sorting for view ingredient

--- src/main/java/fridgy/ui/IngredientDisplay.java
+++ src/main/java/fridgy/ui/IngredientDisplay.java
@@ -53,7 +53,11 @@ public class IngredientDisplay extends UiPart<Region> {
         expiryDate.setText("Expiring on: " + ingredient.getExpiryDate().toString());
         description.setText(ingredient.getDescription().value.orElse(""));
         ingredient.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .sorted(Comparator.comparing(tag ->
+                        tag.tagName.equals("expired") || tag.tagName.equals("expiring")
+                                ? "" // comes first in ordering
+                                : tag.tagName)
+                )
                 .forEach(tag -> {
                     String name = tag.tagName;
                     Label tagLabel = new Label(UiUtil.truncateText(name, TAG_CHAR_LIMIT));
