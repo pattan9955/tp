d7349c098ead10e716d6cc151a581236cd0856a0
bailin <rkxuanfeng@gmail.com>
2021-11-03 14:17:26 +0800

    Fix delete command will shift the selected item and clear command not clearing window when need. Fix #211

--- src/main/java/fridgy/ui/UiState.java
+++ src/main/java/fridgy/ui/UiState.java
@@ -2,6 +2,7 @@ package fridgy.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import javax.management.StandardMBean;
 import fridgy.model.ingredient.Ingredient;
 import fridgy.model.recipe.Recipe;
 
@@ -50,7 +51,9 @@ public class UiState {
     public void delete(Ingredient target) {
         if (target.equals(activeIngredient)) {
             activeIngredient = null;
-            mainWindow.clearWindow();
+            if (mainWindow != null) {
+                mainWindow.clearWindow();
+            }
         }
     }
 
@@ -60,7 +63,22 @@ public class UiState {
     public void delete(Recipe target) {
         if (target.equals(activeRecipe)) {
             activeRecipe = null;
-            mainWindow.clearWindow();
+            if (mainWindow != null) {
+                mainWindow.clearWindow();
+            }
+        }
+    }
+
+    /**
+     * Clears active window if active item is from the list that was recently cleared.
+     */
+    public void clear(TabEnum tab) {
+        if (mainWindow != null) {
+            if (tab == TabEnum.RECIPE && activeRecipe != null) {
+                mainWindow.clearWindow();
+            } else if (tab == TabEnum.INGREDIENT && activeIngredient != null) {
+                mainWindow.clearWindow();
+            }
         }
     }
 
