5c66f061b2871cdc14cacf47a7be0eed217951c0
NayLin-H99 <jamesliu09@gmail.com>
2021-11-04 00:13:03 +0800

    Reject 'expiry' or 'expiring' tags as userinput

--- src/main/java/fridgy/model/tag/Tag.java
+++ src/main/java/fridgy/model/tag/Tag.java
@@ -10,7 +10,8 @@ import fridgy.commons.util.AppUtil;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Tags should be alphanumeric, and should not be related to" +
+            " expiry dates. e.g. expired, expiring";
     public static final String VALIDATION_REGEX = "^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$";
     public static final Tag EXPIRED = new Tag("expired");
     public static final Tag EXPIRING = new Tag("expiring");
@@ -36,6 +37,15 @@ public class Tag {
      */
     public static boolean isValidTagName(String test) {
         return test.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns true if a given string matches with either "expired" or "expiring".
+     * @param test string to be checked
+     * @return
+     */
+    public static boolean isExpiryRelated(String test) {
+        return test.equalsIgnoreCase(EXPIRED.getTag()) || test.equalsIgnoreCase(EXPIRING.getTag());
     }
 
     @Override
